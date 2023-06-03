f79e577981258c1b3105ce92c7c0e711dffdbc3c
loulicc <132032357+loulicc@users.noreply.github.com>
2023-06-03 18:49:56 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -1,30 +1,30 @@
 $(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
-      $(".folder").addClass("fade-out");
+      $(".marquee-1").addClass("fade-out");
     } else {
-      $(".folder").removeClass("fade-out");
+      $(".marquee-1").removeClass("fade-out");
     }
   });
 
-  var originalTop = $(".header").offset().top;
+  var originalTop = $(".marquee").offset().top;
 
   $(window).scroll(function () {
     if ($(window).scrollTop() > 100) {
-      $(".header").addClass("fixed");
+      $(".marquee").addClass("fixed");
     } else {
-      $(".header").removeClass("fixed");
+      $(".marquee").removeClass("fixed");
     }
   });
   
   $(window).scroll(function () {
     if ($(window).scrollTop() > 500) {
-      $(".header.fixed").animate({ top: "0" }, 200);
+      $(".marquee.fixed").animate({ top: "0" }, 200);
     } else {
-      $(".header.fixed").stop().css("top", "5vh");
+      $(".marquee.fixed").stop().css("top", "5vh");
     }
     
     if ($(window).scrollTop() == 0) {
-      $(".header").animate({ top: originalTop + "px" }, 200);
+      $(".marquee").animate({ top: originalTop + "px" }, 200);
     }
   });
   
